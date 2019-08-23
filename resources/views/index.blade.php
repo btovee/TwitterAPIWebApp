783f7a42da53185b2770c698f763acc78c943367
Ben Tovee <ben.tovee@gmail.com>
2019-08-23 15:50:00 +0100

    updated for secure assets

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <script>window.laravel={ csrfToken: '{{ csrf_token() }}' }</script>
-        <link href="{{asset('css/app.css')}}" rel="stylesheet" />
+        <link href="{{secure_asset('css/app.css')}}" rel="stylesheet" />
     </head>
     <body>
 
@@ -19,6 +19,6 @@
 
 
 
-        <script src="{{ asset('js/app.js') }}"></script>
+        <script src="{{ secure_asset('js/app.js') }}"></script>
     </body>
 </html>
